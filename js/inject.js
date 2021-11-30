1d10785dc9ecb9573cdca1618b47f1f057b36a5e
runoobjerry <realjerry.ishere@gmail.com>
2021-11-30 19:39:26 +0800

    1

---
+++ js/inject.js
@@ -0,0 +1,1 @@
+$(window).scroll(function(e){var scrollSeviye=$(this).scrollTop();if(scrollSeviye>500){scrollSeviye=500}var buyutme=(scrollSeviye/100)+1;$('#blur').css('backdrop-filter','blur('+scrollSeviye/100+'px)');$('#blur').css('-webkit-backdrop-filter','blur('+scrollSeviye/100+'px)')});
